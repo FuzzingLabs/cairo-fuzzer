ee61cae39adb15f801f024a40d84a655bb08b1fa
Nabih <nabih@fuzzinglabs.com>
2022-11-24 03:06:58 +0100

    Add parsing of json file so we can get functions informations. We can now iterate over every functions and execute it

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,38 @@
 use cairo_rs::types::program::Program;
 use std::path::Path;
-use cairo_rs::cairo_run::write_output;
 use cairo_rs::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::BuiltinHintProcessor;
 use cairo_rs::types::relocatable::MaybeRelocatable;
 use num_bigint::BigInt;
 use cairo_rs::vm::runners::cairo_runner::CairoRunner;
 use cairo_rs::vm::vm_core::VirtualMachine;
 use num_bigint::Sign;
-
-macro_rules! bigint {
-    ($val : expr) => {
-        Into::<BigInt>::into($val)
-    };
-}
-
-macro_rules! mayberelocatable {
-    ($val1 : expr, $val2 : expr) => {
-        MaybeRelocatable::from(($val1, $val2))
-    };
-    ($val1 : expr) => {
-        MaybeRelocatable::from((bigint!($val1)))
-    };
-}
-macro_rules! cairo_runner {
-    ($program:expr) => {
-        CairoRunner::new(&$program, "all", false).unwrap()
-    };
-    ($program:expr, $layout:expr) => {
-        CairoRunner::new(&$program, $layout, false).unwrap()
-    };
-    ($program:expr, $layout:expr, $proof_mode:expr) => {
-        CairoRunner::new(&$program, $layout, $proof_mode).unwrap()
-    };
-    ($program:expr, $layout:expr, $proof_mode:expr) => {
-        CairoRunner::new(&program, $layout.to_string(), proof_mode).unwrap()
-    };
-}
-macro_rules! vm {
-    () => {{
-        VirtualMachine::new(
-            BigInt::new(Sign::Plus, vec![1, 0, 0, 0, 0, 0, 17, 134217728]),
-            false,
-        )
-    }};
-
-    ($use_trace:expr) => {{
-        VirtualMachine::new(
-            BigInt::new(Sign::Plus, vec![1, 0, 0, 0, 0, 0, 17, 134217728]),
-            $use_trace,
-        )
-    }};
-}
+mod parse_json;
+use crate::parse_json::parse_json;
+mod utils;
 
 fn runner(file_path: String, func_name: String) {
+    println!("====> Running function : {}", func_name);
+    println!("");
     let program =
         Program::from_file(Path::new(&file_path), Some(&func_name)).unwrap();
     let mut cairo_runner = cairo_runner!(program);
     let mut vm = vm!();
     let hint_processor = BuiltinHintProcessor::new_empty();
 
-    let entrypoint = program
+    let entrypoint = match program
         .identifiers
         .get(&format!("__main__.{}", &func_name))
         .unwrap()
-        .pc
-        .unwrap();
+        .pc {
+            Some(value) => value,
+            None => return,
+        };
 
-    //cairo_runner.initialize(&mut vm).unwrap();
-    cairo_runner.initialize_function_runner(&mut vm).unwrap();
-    //vm.accessed_addresses = Some(Vec::new());
-    //cairo_runner.initialize_function_runner(&mut vm).unwrap();
     cairo_runner.initialize_builtins(&mut vm).unwrap();
-    //cairo_runner.initialize_segments(&mut vm, None);
+    cairo_runner.initialize_segments(&mut vm, None);
     let _var = cairo_runner.run_from_entrypoint(
             entrypoint,
-            vec![&mayberelocatable!(123456)],
+            vec![&MaybeRelocatable::from((2,0))],
             false,
             true,
             true,
@@ -84,48 +42,15 @@ fn runner(file_path: String, func_name: String) {
 
     let mut stdout = Vec::<u8>::new();
     cairo_runner.write_output(&mut vm, &mut stdout).unwrap();
-    //assert_eq!(String::from_utf8(stdout), Ok(String::from("1\n17\n")));
-    println!("{:?}", stdout); 
-
-
-    //write_output(&mut cairo_runner, &mut vm).unwrap();
-    //cairo_runner.run_ended = true;
-    //cairo_runner.end_run(false, true, &mut vm, &hint_processor);
-    //cairo_runner.finalize_segments(&mut vm).unwrap();
-    println!("{:?}", cairo_runner.get_output(&mut vm).unwrap()); 
-    //cairo_runner.read_return_values(&mut vm).unwrap();
+    println!("write output : {:?}", stdout); 
+    println!("");
+    println!("get output : {:?}", cairo_runner.get_output(&mut vm).unwrap());
+    println!(""); 
 }
 
 fn main() {
-
-
-/*
-%builtins output
-
-from starkware.cairo.common.serialize import serialize_word
-
-func return_10() -> (res : felt):
-    let res = 10
-    return (res)
-end
-
-func main{output_ptr : felt*}():
-    
-    let (value) = return_10()
-
-    serialize_word(value)
-
-    return ()
-end
-
-program=json/cairo_function_return_to_variable.json
-*/
-
-    // get return value
-    let args = vec![&mayberelocatable!(123456)];
-    runner("json/cairo_function_return_to_variable.json".to_string(), "return_10".to_string());
-
-    println!("======");
-    // get output_ptr
-    runner("json/cairo_function_return_to_variable.json".to_string(), "main".to_string());
+    let functions = parse_json("json/vuln.json".to_string());
+    for function in functions {
+    runner("json/vuln.json".to_string(), function.name);
+    }
 }
---
+++ src/parse_json.rs
@@ -0,0 +1,41 @@
+use serde_json::{Result, Value};
+use std::fs;
+
+#[derive(Debug)]
+pub struct Function {
+    pub name: String,
+    pub num_args: u64,
+    pub type_args: Vec<String>,
+}
+
+pub fn get_type_args(members : &Value) -> Vec<String> {
+    let mut type_args = Vec::<String>::new();
+    for (_key, value) in members.as_object().unwrap() {
+        type_args.push(value["cairo_type"].to_string());
+    }
+    return type_args;
+}
+
+pub fn parse_json(filename: String) -> Vec<Function> {
+    println!("====> Parsing file {}", filename);
+    println!("");
+    let data = fs::read_to_string(filename).expect("Unable to read file");
+    let data: Value = serde_json::from_str(&data).expect("JSON was not well-formatted");
+    let identifiers = &data["identifiers"];
+    let mut functions: Vec<Function> = Vec::<Function>::new();
+    for (key, value) in identifiers.as_object().unwrap() {
+        if value["type"] == "function" {
+            if let Some(_field) = identifiers.get(format!("{}.Args", key)) {
+                let new_function = Function {
+                    name: key.split(".").last().unwrap().to_string(),
+                    num_args: identifiers[format!("{}.Args", key)]["size"]
+                        .as_u64().unwrap(),
+                    type_args: get_type_args(&identifiers[format!("{}.Args", key)]["members"]),
+                };
+                functions.push(new_function);
+            }
+        }
+    }
+    //println!("{:?}", functions);
+    return functions;
+}
