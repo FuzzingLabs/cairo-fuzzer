4eaf7d0b9184fda77ecabd878150182e971e0d56
Nabih <nabih@fuzzinglabs.com>
2022-11-29 11:05:27 +0100

    Add harnesses for libfuzzer and cargo-libafl

--- fuzz/fuzz_targets/cairo_fuzzer.rs
+++ fuzz/fuzz_targets/cairo_fuzzer.rs
@@ -1,10 +1,14 @@
+//#[macro_use]
+//extern crate honggfuzz;
+#![no_main]
 #[macro_use]
-extern crate honggfuzz;
+//extern crate libfuzzer_sys;
 use cairo_rs::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::BuiltinHintProcessor;
 use cairo_rs::types::program::Program;
 use cairo_rs::types::relocatable::MaybeRelocatable;
 use cairo_rs::vm::runners::cairo_runner::CairoRunner;
 use cairo_rs::vm::vm_core::VirtualMachine;
+use cargo_libafl_helper::fuzz_target;
 use num_bigint::BigInt;
 use num_bigint::Sign;
 use std::any::Any;
@@ -13,11 +17,14 @@ use std::fs;
 use std::path::Path;
 mod parse_json;
 use crate::parse_json::parse_json;
+use crate::parse_json::Function;
 mod utils;
+#[macro_use]
+extern crate lazy_static;
 
 fn runner(json: &String, func_name: String, args_num: u64, data: isize) {
-    println!("====> Running function : {}", func_name);
-    println!("");
+    //println!("====> Running function : {}", func_name);
+    //println!("");
     let program = Program::from_string(json, Some(&func_name)).unwrap();
     let mut cairo_runner = cairo_runner!(program);
     let mut vm = vm!();
@@ -53,15 +60,27 @@ fn runner(json: &String, func_name: String, args_num: u64, data: isize) {
 
     let mut stdout = Vec::<u8>::new();
     cairo_runner.write_output(&mut vm, &mut stdout).unwrap();
-    println!("write output : {:?}", stdout);
+    /*println!("write output : {:?}", stdout);
     println!("");
     println!(
         "get output : {:?}",
         cairo_runner.get_output(&mut vm).unwrap()
     );
-    println!("");
+    println!("");*/
 }
 
+lazy_static! {
+    static ref FUNCTIONS: Vec<Function> = parse_json(&"json/vuln.json".to_string());
+    static ref CONTENTS: std::string::String = fs::read_to_string(&"json/vuln.json".to_string())
+        .expect("Should have been able to read the file");
+}
+
+fuzz_target!(|data: isize| {
+    for function in FUNCTIONS.clone() {
+        runner(&CONTENTS, function.name, function.num_args, 2);
+    }
+});
+/*
 fn main() {
     let functions = parse_json(&"json/vuln.json".to_string());
     let contents = fs::read_to_string(&"json/vuln.json".to_string())
@@ -76,7 +95,7 @@ fn main() {
             }
         });
     }
-}
+} */
 
 /*
 fn main() {
