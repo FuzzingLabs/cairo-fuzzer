b17d29a59e895fb8f2ba4a189551df84d66cf797
Nabih <nabih@fuzzinglabs.com>
2022-12-22 11:43:17 +0100

    clean stats.rs

--- src/fuzzer/stats.rs
+++ src/fuzzer/stats.rs
@@ -31,55 +31,12 @@ pub struct Statistics {
     /// Set of all unique crashes
     pub crash_db: HashSet<FuzzInput>,
 
+    // Number of threads that finished to run
     pub threads_finished: u64,
 }
 
 impl Statistics {
-    pub fn get_input_by_index(&self, index: usize) -> FuzzInput {
-        self.input_list[index].clone()
+    pub fn get_input_by_index(&self, index: usize) -> &FuzzInput {
+        &self.input_list[index]
     }
 }
-
-/* pub fn print_stats(fuzzing_data: &Arc<FuzzingData>, replay: bool, workers: usize) {
-    let mut log = None;
-    if fuzzing_data.logs {
-        log = Some(File::create("fuzz_stats.txt").unwrap());
-    }
-    loop {
-        std::thread::sleep(Duration::from_millis(1000));
-
-        // Get access to the global stats
-        let stats = fuzzing_data.stats.lock().unwrap();
-
-        let uptime = (Instant::now() - fuzzing_data.start_time).as_secs_f64();
-        let fuzz_case = stats.fuzz_cases;
-        print!(
-            "{:12.2} uptime | {:9} fuzz cases | {:12.2} fcps | \
-                    {:6} coverage | {:6} inputs | {:6} crashes [{:6} unique]\n",
-            uptime,
-            fuzz_case,
-            fuzz_case as f64 / uptime,
-            stats.coverage_db.len(),
-            stats.input_len,
-            stats.crashes,
-            stats.crash_db.len()
-        );
-        if let Some(ref mut file) = log {
-            write!(
-                file,
-                "{:12.0} {:7} {:8} {:5} {:6} {:6}\n",
-                uptime,
-                fuzz_case,
-                stats.coverage_db.len(),
-                stats.input_len,
-                stats.crashes,
-                stats.crash_db.len()
-            )
-            .unwrap();
-            file.flush().unwrap();
-        }
-        if replay && stats.threads_finished == workers as u64 {
-            break;
-        }
-    }
-} */
