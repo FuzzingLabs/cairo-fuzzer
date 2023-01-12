57b23f8cbdde009f26bab1e24fead3a891d9e848
Nabih <nabih@fuzzinglabs.com>
2023-01-12 14:38:49 +0100

    add invoke and call wrapper

--- src/POC/main.rs
+++ src/POC/main.rs
@@ -6,6 +6,10 @@ use std::process::Output;
 use std::sync::Arc;
 use std::thread;
 
+pub struct Contract {
+    address: String,
+}
+
 pub fn display_tx_status(hash: &String, port: &String) {
     let address = format!("http://127.0.0.1:{}", port).to_string();
     let status = Command::new("starknet")
@@ -98,7 +102,7 @@ pub fn display_output(out: Output) {
     println!("STDERR =>{}", out_to_str);
 }
 
-pub fn deploy_contract(port: &String, rnd_account: &String, contract_path: &String) {
+pub fn deploy_contract(port: &String, rnd_account: &String, contract_path: &String) -> Contract {
     let address = format!("http://127.0.0.1:{}", port).to_string();
     let declare_contract = Command::new("starknet")
         .env(
@@ -117,7 +121,7 @@ pub fn deploy_contract(port: &String, rnd_account: &String, contract_path: &Stri
         .arg(&address)
         .output()
         .expect("failed to execute process");
-    //display_output(declare_contract.clone());
+
     let declare_hash = &get_tx_hash(declare_contract.clone());
     if check_tx_status(declare_hash, port) {
         let class_hash = get_class_hash(declare_contract.clone());
@@ -143,6 +147,10 @@ pub fn deploy_contract(port: &String, rnd_account: &String, contract_path: &Stri
             println!("Error while deploying contract");
             process::exit(1);
         }
+        let contract_address = get_contract_address(deploy_contract.clone());
+        return Contract {
+            address: contract_address
+        };
         //display_tx_status(deploy_hash, port);
     } else {
         println!("Error while declaring contract");
@@ -193,9 +201,68 @@ pub fn deploy_account(port: &String, rnd_account: &String) {
     }
 }
 
+pub fn call_contract(port: &String, rnd_account: &String, abi_path: &String, contract_data: &Contract, function_name: &String) {
+    let address = format!("http://127.0.0.1:{}", port).to_string();
+    let call_contract = Command::new("starknet")
+        .env(
+            "STARKNET_WALLET",
+            "starkware.starknet.wallets.open_zeppelin.OpenZeppelinAccount",
+        )
+        .env("STARKNET_NETWORK", "alpha-goerli")
+        .arg("call")
+        .arg("--address")
+        .arg(&contract_data.address)
+        .arg("--abi")
+        .arg(abi_path)
+        .arg("--function")
+        .arg(function_name)
+        .arg("--account")
+        .arg(&rnd_account)
+        .arg("--feeder_gateway_url")
+        .arg(&address)
+        .arg("--gateway_url")
+        .arg(&address)
+        .output()
+        .expect("failed to execute process");
+    display_output(call_contract.clone());
+}
+
+pub fn invoke_contract(port: &String, rnd_account: &String, abi_path: &String, contract_data: &Contract, function_name: &String, inputs: &String) {
+    let address = format!("http://127.0.0.1:{}", port).to_string();
+    let invoke_contract = Command::new("starknet")
+        .env(
+            "STARKNET_WALLET",
+            "starkware.starknet.wallets.open_zeppelin.OpenZeppelinAccount",
+        )
+        .env("STARKNET_NETWORK", "alpha-goerli")
+        .arg("invoke")
+        .arg("--address")
+        .arg(&contract_data.address)
+        .arg("--abi")
+        .arg(abi_path)
+        .arg("--function")
+        .arg(function_name)
+        .arg("--inputs")
+        .arg(inputs)
+        .arg("--account")
+        .arg(&rnd_account)
+        .arg("--feeder_gateway_url")
+        .arg(&address)
+        .arg("--gateway_url")
+        .arg(&address)
+        .output()
+        .expect("failed to execute process");
+    let invoke_hash = &get_tx_hash(invoke_contract);
+    if !check_tx_status(invoke_hash, port) {
+        println!("Error while invoking contract");
+        process::exit(1);
+    }
+}
+
 pub fn main() {
     let port = "5051";
     let contract_path = &"/home/nabih/cairo-fuzzer/tests/increase_balance.json".to_string();
+    let abi_path = &"/home/nabih/cairo-fuzzer/tests/increase_balance_abi.json".to_string();
     let rnd_account: &String = &rand::thread_rng()
         .sample_iter(&Alphanumeric)
         .take(7)
@@ -213,7 +280,12 @@ pub fn main() {
     });
     deploy_account(&port.to_string(), rnd_account);
     println!("Account deployed successfully");
-    deploy_contract(&port.to_string(), rnd_account, contract_path);
+    let contract_data= deploy_contract(&port.to_string(), rnd_account, contract_path);
     println!("Contract deployed successfully");
+    println!("First call");
+    call_contract(&port.to_string(), rnd_account, abi_path, &contract_data, &"get_balance".to_string());
+    invoke_contract(&port.to_string(), rnd_account, abi_path, &contract_data, &"increase_balance".to_string(), &"1".to_string());
+    call_contract(&port.to_string(), rnd_account, abi_path, &contract_data, &"get_balance".to_string());
+    println!("Contract invoked");
     while true {}
 }
