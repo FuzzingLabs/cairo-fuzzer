1850e7bd92b3779fbf992cc20f2347a9e3e5990d
raefko <nabih@fuzzinglabs.com>
2023-09-06 10:35:26 +0200

    Remove Todo! since the runner takes a felt252 vector, it's a good idea to send random data

--- src/mutator/mutator_felt252.rs
+++ src/mutator/mutator_felt252.rs
@@ -226,7 +226,9 @@ impl Mutator {
                     bits_to_save = 252;
                 }
 
-                _ => todo!(),
+                _ => {
+                    bits_to_save = 252; // Should be a Todo! but since the runner take a felt252 vector, it's a good idea to send random data
+                }
             }
             if bits_to_save != 256 && bits_to_save != 252 {
                 //println!("{:?} - Before {:?}", input_type, self.input[idx]);
