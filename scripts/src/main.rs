7664dcad360ba62f4b289263d1fa9486d09531a4
raefko <nabih@fuzzinglabs.com>
2023-10-17 11:53:28 +0200

    Update scripts for multi contract fuzzing usage. still does not work

--- scripts/src/main.rs
+++ scripts/src/main.rs
@@ -1,7 +1,9 @@
 use cairo_fuzzer::json::json_parser::get_function_from_json;
-use cairo_fuzzer::runner;
+use cairo_fuzzer::runner::runner::Runner;
 use cairo_fuzzer::runner::starknet_runner::RunnerStarknet;
 use cairo_lang_starknet::casm_contract_class::CasmContractClass;
+use felt::Felt252;
+
 use std::fs;
 
 fn main() {
@@ -10,29 +12,33 @@ fn main() {
     let sierra_file = "../tests1.0/fuzzinglabs_init.json";
     let casm_content = fs::read_to_string(casm_file).expect("Could not read casm file");
     let sierra_content = fs::read_to_string(sierra_file).expect("Could not read casm file");
-    let init_function_name = "init_function".to_string();
+    let init_function_name = "init".to_string();
 
     let function = get_function_from_json(&sierra_content, &init_function_name)
         .expect("Could not get function");
     let contract_class: CasmContractClass =
         serde_json::from_str(&casm_content).expect("could not get contractclass");
-    let runner = RunnerStarknet::new(&contract_class, function.selector_idx);
-    runner.run(vec![]);
+    let mut runner = RunnerStarknet::new(&contract_class, function.selector_idx);
+    let input: Vec<Felt252> = vec![Felt252::from_bytes_be(&10000000_i64.to_be_bytes())];
+    runner = runner.clone().run(&input).unwrap().0;
     let state = runner.get_state();
+    println!("================================================");
 
     // Ready to fuzz other contract
     let casm_file = "../tests1.0/fuzzinglabs_fuzz.casm";
     let sierra_file = "../tests1.0/fuzzinglabs_fuzz.json";
     let casm_content = fs::read_to_string(casm_file).expect("Could not read casm file");
     let sierra_content = fs::read_to_string(sierra_file).expect("Could not read casm file");
-    let init_function_name = "fuzz_function".to_string();
+    let init_function_name = "storage_test".to_string();
 
     let function = get_function_from_json(&sierra_content, &init_function_name)
         .expect("Could not get function");
     let contract_class: CasmContractClass =
         serde_json::from_str(&casm_content).expect("could not get contractclass");
 
-    let runner = RunnerStarknet::new(&contract_class, function.selector_idx);
-    runner.set_state(state);
-    // Need to fix Fuzzer class for lib usage or create another constructor
+    let mut runner = RunnerStarknet::new(&contract_class, function.selector_idx);
+    runner = runner.clone().set_state(state.cache);
+    let state = runner.clone().get_state();
+    let input: Vec<Felt252> = vec![];
+    runner = runner.clone().run(&input).unwrap();
 }
